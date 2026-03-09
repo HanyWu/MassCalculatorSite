a6aad1a8de64231170b3875cc5b9a57a257241bf
HanyWu <85171140+HanyWu@users.noreply.github.com>
2026-03-09 20:01:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MassCalculatorSite/', // ★ GitHub 仓库名，区分大小写
 })
